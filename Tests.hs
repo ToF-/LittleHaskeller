053a66c5d2119a7d24253dff23cbae4ecef88ebd
Christophe Thibaut <cthibaut@octo.com>
2011-10-25 22:43:07 +0200

    changed letters into suit symbols in the code

--- Tests.hs
+++ Tests.hs
@@ -5,14 +5,14 @@ import PokerHand
 import Data.Ord (comparing)
 import Data.List (sort,sortBy)
 
-ud = words "Ah 2h Th Kh 9h Qh Jh"
-sd = words "2h 9h Th Jh Qh Kh Ah"
+ud = words "A♥ 2♥ T♥ K♥ 9♥ Q♥ J♥"
+sd = words "2♥ 9♥ T♥ J♥ Q♥ K♥ A♥"
 
 main = runTestTT $ TestList 
        [sortBy (comparing card) ud  ~?= sd
-       ,map suit (cards "Ac Ad Ah As") ~?= ['c','d','h','s']
-       ,flush (cards "Ah Th 3h 4h 2h") ~?= True
-       ,flush (cards "Ad Th 3h 4h 2s") ~?= False
-       ,flush (cards "Ad Td 3d 4d 2d") ~?= True]
+       ,map suit (cards "A♣ A♦ A♥ A♠") ~?= ['♣','♦','♥','♠']
+       ,flush (cards "A♥ T♥ 3♥ 4♥ 2♥") ~?= True
+       ,flush (cards "A♦ T♥ 3♥ 4♥ 2s") ~?= False
+       ,flush (cards "A♦ T♦ 3♦ 4♦ 2♦") ~?= True]
     where cards = map card . words
 
