41b89bfb4727ba54ca0946ff57e49b35cbca9e35
Christophe Thibaut <cthibaut@octo.com>
2011-10-26 10:44:45 +0200

    added a refactoring step. Display the code at the end of chapter 3

--- PokerHand.hs
+++ PokerHand.hs
@@ -2,26 +2,23 @@ module PokerHand
 where
 import Char
 
-data Card = C Value Suit deriving (Ord,Eq)
+data Card = C { value :: Value, suit :: Suit } 
+            deriving (Ord,Eq)
 type Value = Int
-
+type Suit = Char
 
 card :: String -> Card
-card [v,s] = C (value v) s
+card [v,s] = C (toValue v) s
     where 
-      value 'A' = 14
-      value 'K' = 13
-      value 'Q' = 12
-      value 'J' = 11
-      value 'T' = 10
-      value  c  = ((ord c) - (ord '0'))
+      toValue 'A' = 14
+      toValue 'K' = 13
+      toValue 'Q' = 12
+      toValue 'J' = 11
+      toValue 'T' = 10
+      toValue  c  = ((ord c) - (ord '0'))
 
 flush :: [Card] -> Bool
 flush (c:cs) = all (\x -> suit x == suit c) cs
 
-type Suit = Char
-
-suit :: Card -> Suit
-suit (C _ s) = s
 
 
--- Tests.hs
+++ Tests.hs
@@ -5,14 +5,14 @@ import PokerHand
 import Data.Ord (comparing)
 import Data.List (sort,sortBy)
 
-ud = words "A♥ 2♥ T♥ K♥ 9♥ Q♥ J♥"
-sd = words "2♥ 9♥ T♥ J♥ Q♥ K♥ A♥"
+ud = words "A♣ 2♣ T♣ K♣ 9♣ Q♣ J♣"
+sd = words "2♣ 9♣ T♣ J♣ Q♣ K♣ A♣"
 
 main = runTestTT $ TestList 
        [sortBy (comparing card) ud  ~?= sd
        ,map suit (cards "A♣ A♦ A♥ A♠") ~?= ['♣','♦','♥','♠']
-       ,flush (cards "A♥ T♥ 3♥ 4♥ 2♥") ~?= True
-       ,flush (cards "A♦ T♥ 3♥ 4♥ 2♥") ~?= False
-       ,flush (cards "A♦ T♦ 3♦ 4♦ 2♦") ~?= True]
+       ,flush (cards "A♣ T♣ 3♣ 4♣ 2♣") ~?= True
+       ,flush (cards "A♠ T♣ 3♣ 4♣ 2♣") ~?= False
+       ,flush (cards "A♠ T♠ 3♠ 4♠ 2♠") ~?= True]
     where cards = map card . words
 
